fbeacd06436df79f14f1ce725ede9e4fd477eb9e
monxchrome <98259661+monxchrome@users.noreply.github.com>
2022-02-14 13:35:52 +0200

    social

--- blog/models.py
+++ blog/models.py
@@ -2,20 +2,23 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from users.models import User
 
-class Media()
+class Media():
+    size = models.PositiveIntegerField(_('file size'), null = True, blank=True)
+    name = models.CharField(_('file name'), max_length=255, null=True, blank=True)
 
-class VideoMedia(models.Model):
+    class Meta:
+        abstract = True
+
+class VideoMedia(Media):
     file = models.FileField(upload_to='videos/')
 
-class PhotoMedia(models.Model):
+class PhotoMedia(Media):
     file = models.ImageField(upload_to='photos/')
-    size = models.PositiveIntegerField(_('file size')(null=True, blank=True))
-    name = models.CharField(_('file name'), max_length= 255, null=True, blank=True)
+
+class MediaSection(models.Model):
 
 
 class Posts(models.Model):
-    title = models.CharField(_('title'),max_length=70)
-    slug = models.SlugField(editable=False, null=True, blank=True)
 
     STATUSES = (
         (1, _('draft')),
@@ -24,18 +27,21 @@ class Posts(models.Model):
         (3, _('published')),
     )
 
-    status = models.PositiveSmallIntegerField(_('status')(choices=STATUSES, default=1))
-    tags = models.JSONField(_('tags')(null=True, blank=True))
-
     TYPES = (
         (1, _('text post')),
         (2, _('photo post')),
         (3, _('video post')),
     )
 
+    title = models.CharField(_('title'), max_length=70)
+    slug = models.SlugField(editable=False, null=True, blank=True)
+    status = models.PositiveSmallIntegerField(_('status')(choices=STATUSES, default=1))
+    tags = models.JSONField(_('tags')(null=True, blank=True))
     type = models.PositiveSmallIntegerField(_('type')(choices=TYPES))
     content = models.TextField(_('content')(null=True, blank=True))
     views = models.PositiveIntegerField(_('views'), default=0)
     created_at = models.DateTimeField(_('created at'),(auto_now_add=True))
     published_at = models.DateTimeField(_('published at'),(null=True, blank=True))
-    author = models.ForeignKey(_('author')(User, on_delete=models.CASCADE, related_name='posts'))
+    author = models.ForeignKey(_('author'),User, on_delete=models.CASCADE, related_name='posts')
+    photo = models.OneToOneField(_('photo'),PhotoMedia, null=True, blank=True)
+    video = models.OneToOneField(_('video'), VideoMedia, null=True, blank=True)
--- users/models.py
+++ users/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
 
 class User(AbstractBaseUser):
-    date_of_birth
+    date_of_birth = models.DateField()
+    height = models.FloatField()
+
+    REQUIRED_FIELDS = ['date_of_birth', 'height']
