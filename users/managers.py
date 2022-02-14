7d5649bca87c3fd0cc0c9ce0494beb946c910a8a
monxchrome <98259661+monxchrome@users.noreply.github.com>
2022-02-14 19:53:59 +0200

    admin

---
+++ users/managers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self._create_super_user(email, password, **extra_fields)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
+import uuid
+from users.managers import UserManager
+from django.utils.translation import gettext_lazy as _
 
 class User(AbstractBaseUser):
-    date_of_birth = models.DateField()
-    height = models.FloatField()
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    username = models.CharField(max_length=255, unique=True, db_index=True)
+    email = models.EmailField(null=True, blank=True)
+    first_name = models.CharField(max_length=255, db_index=True, null=True, blank=True)
+    last_name = models.CharField(max_length=255, db_index=True, null=True, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    phones = models.CharField(null=True, blank=True, verbose_name=('phone'), max_length=32)
+    is_staff = models.BooleanField(default=False, verbose_name=_('teacher'))
 
-    REQUIRED_FIELDS = ['date_of_birth', 'height']
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager
